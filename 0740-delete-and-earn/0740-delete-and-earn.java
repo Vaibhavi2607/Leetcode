91d9c80b7d9b6d1af49cf3074cfdbf0acd0e09fa
Vaibhavi2607 <128307853+Vaibhavi2607@users.noreply.github.com>
2024-07-23 09:20:18 +0530

    Time: 4 ms (75.01%), Space: 44.3 MB (62.1%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,18 +1,18 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        Map<Integer, Integer> points = new HashMap<>();
-        int maxNum = 0;
-        for (int num : nums) {
-            points.put(num, points.getOrDefault(num, 0) + num);
-            maxNum = Math.max(maxNum, num);
-        }
+        int maxNum = Arrays.stream(nums).max().orElse(0);
+    int[] points = new int[maxNum + 1];
+    for (int num : nums) {
+        points[num] += num;
+    }
 
-        int[] dp = new int[maxNum + 1];
-        dp[0] = points.getOrDefault(0, 0);
-        if (maxNum > 0) dp[1] = points.getOrDefault(1, 0);
-        for (int i = 2; i <= maxNum; i++)
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + points.getOrDefault(i, 0));
-        
-        return dp[maxNum];
+    int prev1 = 0, prev2 = 0;
+    for (int i = 0; i <= maxNum; i++) {
+        int curr = Math.max(prev1, prev2 + points[i]);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
     }
 }
