ff352fed6183da354d31a4719311ee37096594c7
Vaibhavi2607 <128307853+Vaibhavi2607@users.noreply.github.com>
2025-01-11 22:58:27 +0530

    Time: 119 ms (41.18%), Space: 13 MB (35.29%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -1,24 +1,11 @@
 class Solution(object):
     def canConstruct(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: bool
-        """
         if len(s) < k:
             return False
-        sorted_s = sorted(s)
         
-        odd_count = 0
-        i = 0
-        
-        while i < len(sorted_s):
-            char = sorted_s[i]
-            count = 0
-            while i < len(sorted_s) and sorted_s[i] == char:
-                count += 1
-                i += 1
-            if count % 2 == 1:
-                odd_count += 1
-        
+        freq = Counter(s)
+
+        odd_count = sum(1 for count in freq.values() if count%2==1)
+
         return odd_count <= k
+    
