fb54a4881a6d18688e398c03e7cb4cab081c3e45
Vaibhavi2607 <128307853+Vaibhavi2607@users.noreply.github.com>
2025-08-17 10:49:55 +0530

    Time: 23 ms (56.63%), Space: 13.6 MB (31.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,9 +8,7 @@ class Solution(object):
         if len(s) != len(t):
             return False
 
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        return sorted(s) == sorted(t)
 
         
         
